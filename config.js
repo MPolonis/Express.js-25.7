a08feca7cd18320ac556e3720734e19171ca9496
Marcin Polonis <marcin.polonis@o2.pl>
2019-04-04 21:47:56 +0200

    Start doing task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '1095826829686-i21k6mk34jmco10uo5mhkk6o3rdfslho.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '0r4WABSzOz4Sp3h0klfBSsSM',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
